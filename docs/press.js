6353d3f2a75eaa6948071d7040630fcde90282fa
amer musa <amer.99.er@gmail.com>
2025-08-02 00:14:55 +0200

    Update press.js
    Signed-off-by: amer musa <amer.99.er@gmail.com>

--- docs/press.js
+++ docs/press.js
@@ -1,16 +1,7 @@
 document.querySelectorAll('.language-title').forEach(title => {
   title.addEventListener('click', () => {
+    title.classList.toggle('open');
     const section = title.nextElementSibling;
-    const isOpen = section.classList.contains('open');
-
-    // Close all
-    document.querySelectorAll('.language-section').forEach(sec => sec.classList.remove('open'));
-    document.querySelectorAll('.language-title').forEach(t => t.classList.remove('open'));
-
-    // Open clicked if it was not already open
-    if (!isOpen) {
-      section.classList.add('open');
-      title.classList.add('open');
-    }
+    section.classList.toggle('open');
   });
 });
