ad7661b0f8450a40ecd1c12f03930c32a9128654
amer musa <amer.99.er@gmail.com>
2025-08-01 21:34:58 +0200

    Update press.js
    Signed-off-by: amer musa <amer.99.er@gmail.com>

--- docs/press.js
+++ docs/press.js
@@ -1,1 +1,7 @@
-jjb
+document.querySelectorAll('.language-title').forEach(title => {
+  title.addEventListener('click', () => {
+    title.classList.toggle('open');
+    const section = title.nextElementSibling;
+    section.classList.toggle('open');
+  });
+});
