bd5998fa6b3b44b3ade8c2a5822676c9b626bd29
amer musa <amer.99.er@gmail.com>
2025-08-01 23:24:35 +0200

    Delete docs/press[1].js
    Signed-off-by: amer musa <amer.99.er@gmail.com>

--- docs/press[1].js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.language-title').forEach(title => {
-  title.addEventListener('click', () => {
-    title.classList.toggle('open');
-    const section = title.nextElementSibling;
-    section.classList.toggle('open');
-  });
-});
